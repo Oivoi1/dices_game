b4a75df1f43b573bb5eaeb97a354106030ec2010
Veeti Oivo <n1oive00@students.oamk.fi>
2023-09-12 21:28:03 +0300

    first commit

---
+++ style/style.js
@@ -0,0 +1,68 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  header: {
+    marginTop: 40,
+    marginBottom: 15,
+    backgroundColor: "#d88e2c",
+    flexDirection: 'row',
+  },
+  footer: {
+    marginTop: 20,
+    backgroundColor: "#d88e2c",
+    flexDirection: 'row'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  author: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  gameboard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gameinfo: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    marginTop: 10
+  },
+  row: {
+    marginTop: 20,
+    padding: 10
+  },
+  flex: {
+    flexDirection: "row"
+  },
+  button: {
+    margin: 30,
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "#d6ab73",
+    width: 150,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color:"#52482b",
+    fontSize: 20
+  }
+});
